e59c8a44a4cb729599e86e8ab68808f63b4e6aba
robertjosephwayne <robertjwayne@gmail.com>
2017-04-27 12:23:33 -0400

    Clean up formatting

--- app.rb
+++ app.rb
@@ -1,19 +1,19 @@
+# Application controller
 class App < Sinatra::Base
+  get '/' do
+    erb :index
+  end
 
-	get '/' do
-		erb :index
-	end
+  get '/hello' do
+    erb :hello
+  end
 
-	get '/hello' do
-		erb :hello
-	end
+  get '/goodbye' do
+    erb :goodbye
+  end
 
-	get '/goodbye' do
-		erb :goodbye
-	end
-
-	get '/date' do
-		erb :date
-	end
+  get '/date' do
+    erb :date
+  end
 
 end
